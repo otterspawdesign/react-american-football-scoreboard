d1a19291b251fab7dc3c49153d0ec1bec9b93121
Melissa Kemp <otterspawdesign@gmail.com>
2019-11-13 18:39:43 -0700

    changed CSS to powderpuff colors and set useState to 0

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+let [homeScore, setHomeScore] = useState(0);
+let [awayScore, setAwayScore] = useState(0);
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Knights</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -19,7 +20,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Bees</h2>
             <div className="away__score">32</div>
           </div>
         </div>
